5b76759273d25390527c8d9aed0f35402021c9e4
Bogdan Daniel Ioan <bogdan.daniel.ioan@outlook.com>
2023-06-19 12:06:18 +0300

    updated jenkins file

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,39 @@
+pipeline {
+
+    agent any
+
+	tools {
+        maven "MAVEN3"
+        jdk  "oraclejdk11"
+    }
+
+    environment {
+        SNAP_REPO = "onlineschoool-snapshot"
+        NEXUS_USER= "ADMIN"
+        NEXUS_PASS="onlineschool"
+        RELEASE_REPO = "onlineschoool-release"
+        CENTRAL_REPO = "onlineschoool-maven-central"
+        NEXUSIP = "172.31.31.64"
+        NEXUSPORT = "9000"
+        NEXUS_GRP_REPO= "onlineschool-group"
+        NEXUS_LOGIN = "nexuslogin"
+
+
+
+    }
+
+    stages{
+
+        stage('BUILD'){
+            steps {
+                sh 'mvn clean install -DskipTests'
+            }
+        }
+
+      
+
+
+    }
+
+
+}
